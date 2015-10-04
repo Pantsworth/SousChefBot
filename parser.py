4976d7d7bf281259736f746d47ebad38d00c49b2
Pantsworth <nski6m@gmail.com>
2015-10-04 15:16:16 -0500

    removed test commit print

--- parser.py
+++ parser.py
@@ -13,7 +13,6 @@ def parse_recipe(url):
     """
     # url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"
     fn = os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
-    print fn
     recipe_json = subprocess.call([fn,url,"json"])
     print recipe_json
     return recipe_json
@@ -37,3 +36,6 @@ def get_html(url):
 
 
 parse_recipe("http://allrecipes.com/recipe/219173/simple-beef-pot-roast/")
+parse_recipe("http://www.epicurious.com/recipes/food/views/our-favorite-lasagna-with-sausage-spinach-and-three-cheeses-51253440")
+
+
