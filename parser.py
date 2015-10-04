f65ea1cdcc8d4ecb94ba4d2ace9136a691ce698f
Pantsworth <nski6m@gmail.com>
2015-10-04 14:38:56 -0500

    parser function is finished – it outputs a JSON recipe file for manipulation.

--- parser.py
+++ parser.py
@@ -5,22 +5,14 @@
 import os
 import subprocess
 
-def parse_recipe():
+def parse_recipe(url):
+    # url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"
+    fn = os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
+    recipe_json = subprocess.call([fn,url,"json"])
+    print recipe_json
+    return recipe_json
 
-    url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"
-
-    fn = "" + os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
-    print fn
-    called_command = fn + " " + url + " " + "json"
-    subprocess.call(['fn'])
-    print called_command
-    # json_object = subprocess.call(called_command,)
-
-
-    # print json_object
-    return
-
-parse_recipe()
+parse_recipe("http://allrecipes.com/recipe/219173/simple-beef-pot-roast/")
 
 
 
