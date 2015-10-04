3f5bbbad469dc926392dcf39b21eac19e9ae6a8d
Pantsworth <nski6m@gmail.com>
2015-10-04 14:45:24 -0500

    added get_html for future sanitizing of input

--- parser.py
+++ parser.py
@@ -4,15 +4,35 @@
 
 import os
 import subprocess
+import urllib2
 
 def parse_recipe(url):
+    """
+    :param url: URL for recipe.
+    :return: JSON recipe object, formatted by our delightful PHP library.
+    """
     # url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"
     fn = os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
     recipe_json = subprocess.call([fn,url,"json"])
     print recipe_json
     return recipe_json
 
+
+def get_html(url):
+    """
+    Retrieves html text from a string-formatted url
+    :param url: web url string
+    :return: html text
+    """
+    if '://' not in url:
+        formatted_url = 'http://' + url
+    else:
+        formatted_url = url
+    try:
+        return urllib2.urlopen(formatted_url).read()
+    except urllib2.URLError:
+        print "Invalid URL Request"
+        return None
+
+
 parse_recipe("http://allrecipes.com/recipe/219173/simple-beef-pot-roast/")
-
-
-
