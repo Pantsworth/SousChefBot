8be2c945d5ea10a8db61d721c1bb1d30f8d4249d
Pantsworth <nski6m@gmail.com>
2015-10-11 23:47:13 -0500

    improved parser.py – JSON output is now correct and parsable.

--- parser.py
+++ parser.py
@@ -6,9 +6,10 @@ import os
 import subprocess
 import urllib2
 import platform
+import json
 
 def parse_recipe(url):
-    """
+    """Calls CLI and runs our PHP recipe_parser function. Returns JSON
     :param url: URL for recipe.
     :return: JSON recipe object, formatted by our delightful PHP library.
     """
@@ -16,11 +17,11 @@ def parse_recipe(url):
     # url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"
     if system_type == 'Windows':
         fn = os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
-        recipe_json = subprocess.call(['php.exe', fn, url, "json"])
+        recipe_json = subprocess.check_output(['php.exe', fn, url, "json"])
     else:
         fn = os.path.join(os.path.dirname(__file__), 'RecipeParser/bin/parse_recipe')
-        recipe_json = subprocess.call([fn, url, "json"])
-    print recipe_json
+        recipe_json = subprocess.check_output([fn, url, "json"])
+
     return recipe_json
 
 
@@ -41,8 +42,18 @@ def get_html(url):
         return None
 
 
-parse_recipe("http://allrecipes.com/recipe/219173/simple-beef-pot-roast/")
-parse_recipe("http://www.epicurious.com/recipes/food/views/our-favorite-lasagna-with-sausage-spinach-and-three-cheeses-51253440")
+def human_readable(recipe_json):
+    parsed_json = json.loads(recipe_json)
+    print "SUCCESS"
+    print "TITLE: ", parsed_json['title']
+    print "INGREDIENTS: ", parsed_json['ingredients']
+    return
+
+
+
+
+human_readable(parse_recipe("http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"))
+# parse_recipe("http://www.epicurious.com/recipes/food/views/our-favorite-lasagna-with-sausage-spinach-and-three-cheeses-51253440")
 
 # print platform.system()
 
