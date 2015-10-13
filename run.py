dea34f91077c347a642eb9f1d88025967d08ca6b
Pantsworth <nski6m@gmail.com>
2015-10-12 20:09:45 -0500

    appended a filepath for ini.py that should make things work.

--- run.py
+++ run.py
@@ -4,8 +4,4 @@ from project import app
 
 if __name__ == "__main__":
     app.debug = True
-    # Because we did not initialize Flask-SQLAlchemy with an application
-    # it will use `current_app` instead.  Since we are not in an application
-    # context right now, we will instead pass in the configured application
-    # into our `create_all` call.
     app.run()
