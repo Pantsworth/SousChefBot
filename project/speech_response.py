f60962eb15448cd0cde69a93b54b2bcd823dee73
Charles-Cole <cmc.swimmer@gmail.com>
2015-11-11 18:54:32 -0600

    Created basic structure for choose_response

--- project/speech_response.py
+++ project/speech_response.py
@@ -46,4 +46,54 @@ def choose_response(recipe_object, wit_input, kb_object):
     :param kb_object: our knowledge base
     :return: string that is an appropriate speech response
     """
-    return
+    #wit_input = {u'outcomes': [{u'entities': {}, u'confidence': 0.742, u'intent': u'get_time', u'_text': u'Wit.ai thinks you said: yeah'}], u'msg_id': u'5262a8bf-a25a-4183-bc42-a4cd1807e20e', u'_text': u'Wit.ai thinks you said: yeah'}
+    intent = wit_input[u'outcomes'][0][u'intent']
+    response = ""
+    if intent == 'get_end':
+        response = "stopping"
+    elif intent == 'get_ingredient':
+        #TODO
+        response = "error"
+    elif intent == 'get_ingredient_amount':
+        wanted_ingredient = wit_input[u'outcomes'][0][u'entities']
+        list_ingredient = recipe_object.ingredients
+        indices = [i for i, s in enumerate(list_ingredient) if wanted_ingredient in s]
+        if indices:
+            response = list_ingredient[indices[0]]
+        else:
+            response = "could not find ingredient"
+    elif intent == 'get_temperature':
+        #TODO
+        response = "error"
+    elif intent == 'get_time':
+        #TODO
+        response = "error"
+    elif intent=='how_to_use_tool':
+        #TODO
+        response = "error"
+    elif intent=='ingredient_substitute':
+        #TODO
+        response = "error"
+    elif intent=='navigate_back':
+        #TODO
+        response = "error"
+    elif intent=='navigate_forward':
+        #TODO
+        response = "error"
+    elif intent=='read_recipe':
+        #TODO
+        response = "error"
+    elif intent=='start_up':
+        #TODO
+        response = "error"
+    elif intent=='technique_how_to':
+        #TODO
+        response = "error"
+    elif intent=='which_tool':
+        #TODO
+        response = "error"
+    else:
+        #TODO
+        response = "unknown intent"
+        print intent
+    return response
