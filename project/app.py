a2d1a11f1697ff1c5c0ad0979ab776ab399b23b1
christinakim <christinahkim1@gmail.com>
2015-11-19 17:51:04 -0600

    getting rid of json test - results are in recipe.html

--- project/app.py
+++ project/app.py
@@ -55,7 +55,7 @@ def background_thread():
     #text input
   
 
-@app.route('/json_test', methods=['GET', 'POST'])
+@app.route('/')
 def jsonreq():
     global thread
     print "index"
@@ -63,35 +63,20 @@ def jsonreq():
         thread = Thread(target=background_thread)
         thread.daemon = True
         thread.start()
-    """
-    Calls recipe parser and generates a response page.
-    :return: an html page containing scraped recipe info
-    """
-    request.form['url']
 
-    url = request.form['url']               # acquires URL from form.html
+    return render_template('form.html')
+
+@app.route('/recipe')
+def index():
+    url = "http://allrecipes.com/recipe/219173/simple-beef-pot-roast/"        # acquires URL from form.html
     recipe_object = parser.parse_recipe(url)     # parse html with our parser
 
     recipe_title = recipe_object.title
     recipe_yield = recipe_object.servings
     recipe_ingredients = recipe_object.ingredients
     recipe_instructions = recipe_object.instructions
-
-    print recipe_object.ingredients
-    print recipe_object.instructions
-    #
-    # query = request.json_test['query-text']
-    # print query
-
-
-    # print url
-    # print recipe_title, " ", recipe_ingredients
-    return render_template('json_test.html',html_title=recipe_title, html_yield=recipe_yield,
+    return render_template('recipe.html', html_title=recipe_title, html_yield=recipe_yield,
                            html_ingredients=recipe_ingredients, html_instructions=recipe_instructions, jsondata="")
-
-@app.route('/')
-def index():
-    return render_template('form.html')
 
 
 @socketio.on('my event', namespace='/test')
