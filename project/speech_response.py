0d3f6fd2a8efacb90897a2a24ea5670a6cc29d6d
Charles-Cole <cmc.swimmer@gmail.com>
2015-11-23 18:59:02 -0600

    Added simple search for videos

--- project/speech_response.py
+++ project/speech_response.py
@@ -160,7 +160,7 @@ def choose_response(recipe_object, wit_input, kb_object):
         response = "error"
     elif intent=='technique_how_to':
         #TODO
-        query_string = urllib.parse.urlencode({"search_query" : input()})
+        query_string = urllib.parse.urlencode({"search_query": wit_input[u'_text']})
         html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
         search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
         print("http://www.youtube.com/watch?v=" + search_results[0])
