79f49740eefb42bbea5106e15e606609752d0eb4
christinakim <christinahkim1@gmail.com>
2015-11-30 17:50:22 -0600

    creating new socket func for next event

---
+++ project/static/scripts/recipe.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	//only ol is instructions
+	var $list = $("#ingredients li"); 
+	$list.eq(0).addClass("highlight");
+
+	console.log(list.eq(0));
+	//listItems.filter('#0').addClass('.highlight');
+	//need to replace with if intent = next step via socket event
+
+});
